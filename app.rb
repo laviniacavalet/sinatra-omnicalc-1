5cf4640926ba4eb931bfc8c69389faf3890db14e
laviniacavalet <116130863+laviniacavalet@users.noreply.github.com>
2024-04-12 22:00:51 +0000

    added square root

--- app.rb
+++ app.rb
@@ -1,6 +1,36 @@
 require "sinatra"
 require "sinatra/reloader"
 
+get("/howdy") do
+  "hello there"
+end
+
+
+get("/square/new") do
+  erb(:new_square_calc)
+end
+
+get("/square/results") do
+ @the_num = params.fetch("users_number").to_f
+
+ @the_result = @the_num ** 2
+
+  erb(:square_results)
+end
+
+get("/square_root/new") do
+  erb(:new_square_root_calc)
+end
+
+get("/square_root/results") do
+  @the_num = params.fetch("users_number").to_f
+ 
+  @the_result = Math.sqrt(@the_num) 
+ 
+   erb(:square_root_results)
+ end
+
+
 get("/") do
   "
   <h1>Welcome to your Sinatra App!</h1>
