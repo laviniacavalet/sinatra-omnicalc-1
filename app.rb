91e8fdaf0b6c0c5f0036488bbd131e3b77226f10
laviniacavalet <116130863+laviniacavalet@users.noreply.github.com>
2024-04-13 00:02:34 +0000

    added random number

--- app.rb
+++ app.rb
@@ -30,10 +30,19 @@ get("/square_root/results") do
    erb(:square_root_results)
  end
 
+ get("/random/new") do
+  erb(:new_random_calc)
+end
+
+get("/random/results") do
+  @min_value = params.fetch("user_min").to_f
+  @max_value = params.fetch("user_max").to_f
+  
+  @random_number = rand(@min_value..@max_value)
+ 
+   erb(:random_results)
+ end
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:new_square_calc)
 end
